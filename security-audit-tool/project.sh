07138be60ab5a7d856ac6e86cb760da51bced063
Anthony-Ngonde <anto.ngonde@gmail.com>
2026-09-02 08:47:22 -0400

    Added a Current/Root User Status function

--- security-audit-tool/project.sh
+++ security-audit-tool/project.sh
@@ -3,3 +3,28 @@
 echo "============================="
 echo "Automated Security Audit Tool"
 echo "============================="
+
+
+user_root_status() {
+
+echo "========================"
+echo "Current/Root User Status"
+echo "========================"
+
+user=$(whoami)
+
+echo "Current User: $user"
+
+if [ "$EUID" -eq 0  ]
+  then
+    echo "Privilege Level: ROOT USER"
+  else
+    echo "Privilege Level: STANDARD USER"
+
+fi
+
+}
+
+
+user_root_status
+
