992711ed7da5488936076ce216360656dd063cb5
Anthony-Ngonde <anto.ngonde@gmail.com>
2026-09-09 08:12:08 -0400

    Added a List Network Interfaces function

--- network-packet-analyzer/project.sh
+++ network-packet-analyzer/project.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/bash
 
-
 echo "======================="
 echo "Network Packet Analyzer"
 echo "======================="
+
+
+list_network_interfaces() {
+
+echo "======================="
+echo "List Network Interfaces"
+echo "======================="
+
+ip -br link
+
+}
+
+list_network_interfaces
+
