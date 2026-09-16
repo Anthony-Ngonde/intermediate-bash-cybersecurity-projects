5a9bfe4b188f9340c87bc0048bdd34266acd410b
Anthony-Ngonde <anto.ngonde@gmail.com>
2026-09-16 08:22:16 -0400

    Added a Most Active IPs function

--- web-server-analyzer/project.sh
+++ web-server-analyzer/project.sh
@@ -3,3 +3,20 @@
 echo "=============================="
 echo "Web Server Log Threat Analyzer"
 echo "=============================="
+
+
+log_file=test_access.log
+
+
+most_active_ips() {
+
+echo "==============="
+echo "Most Active IPs"
+echo "==============="
+
+awk 'NF > 0 {print $1}' "$log_file" | sort | uniq -c | sort -nr | head -2
+
+
+}
+
+most_active_ips
