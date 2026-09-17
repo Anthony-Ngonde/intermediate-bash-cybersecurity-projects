1b4e7dfc7f8ebb046be3f5b6ffb5b884986b9c49
Anthony-Ngonde <anto.ngonde@gmail.com>
2026-09-17 07:12:00 -0400

    Added a Generate Security Analysis report function

--- web-server-analyzer/project.sh
+++ web-server-analyzer/project.sh
@@ -89,7 +89,89 @@ grep -Ei 'curl|Wget|python-requests|sqlmap' "$log_file"
 
 }
 
+generate_security_analysis() {
 
+{
+
+
+echo "====================="
+echo "WEB SECURITY ANALYSIS"
+echo "====================="
+echo
+echo "Generated: $current_date"
+
+
+echo
+echo "======================="
+echo "Total Requests Analyzed"
+echo "======================="
+
+total_count=$(cat "$log_file" | wc -l)
+
+echo "Requests Analyzed: $total_count"
+
+
+echo
+echo "================="
+echo "Most Active IPs"
+echo "================="
+
+awk 'NF > 0 {print $1}' "$log_file" | sort | uniq -c | sort -nr | head -2
+
+
+echo
+echo "==============="
+echo "404 Errors"
+echo "==============="
+
+awk '$9 == 404' "$log_file"
+
+
+echo
+echo "==============="
+echo "403 Errors"
+echo "==============="
+
+awk '$9 == 403' "$log_file"
+
+
+echo
+echo "=================="
+echo "Repeated Requests"
+echo "=================="
+
+awk 'NF > 0 {print $7}' "$log_file" | sort | uniq -c | sort -nr | head -2
+
+
+echo
+echo "==========================="
+echo "Suspicious Request Patterns"
+echo "==========================="
+
+grep -Ei '\.\./|<script>|union.*select' "$log_file"
+
+
+echo
+echo "==========================="
+echo "Requests to Sensitive Paths"
+echo "============================"
+
+grep -Ei 'admin|env|git|phpmyadmin' "$log_file"
+
+
+echo
+echo "===================="
+echo "Unusual User Agents"
+echo "==================="
+
+grep -Ei 'curl|Wget|python-requests|sqlmap' "$log_file"
+
+
+
+
+}
+
+}
 
 
 while true
@@ -104,7 +186,8 @@ echo "4.Repeated Requests"
 echo "5.Suspicious Request Patterns"
 echo "6.Requests to Sensitive Paths"
 echo "7.Unusual User Agents"
-echo "8.Exit"
+echo "8.Generate Security Analysis Report"
+echo "9.Exit"
 
 echo
 read -p "Enter your choice: " choice
@@ -141,6 +224,10 @@ case $choice in
  ;;
 
 8)
+ generate_security_analysis
+ ;;
+
+9)
  echo "Goodbye!"
  exit
 
