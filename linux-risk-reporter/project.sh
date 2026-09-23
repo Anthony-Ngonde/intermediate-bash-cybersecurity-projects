fcc9d2eb2df2238dca27000bac60f5ff76b2a274
Anthony-Ngonde <anto.ngonde@gmail.com>
2026-09-23 08:29:29 -0400

    Added a Operating System function

--- linux-risk-reporter/project.sh
+++ linux-risk-reporter/project.sh
@@ -4,3 +4,19 @@
 echo "=============================================="
 echo "Linux Vulnerability Assessment & Risk Reporter"
 echo "=============================================="
+
+
+operating_system() {
+
+echo "================="
+echo "Operating System"
+echo "================"
+
+cat /etc/os-release |
+grep "PRETTY_NAME"
+
+
+}
+
+
+operating_system
